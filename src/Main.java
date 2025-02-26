6ddd5f7996aebb394b1358f64d0c2b8d4b270192
YulianDiaz <yulian.diaz-g@uniminuto.edu.co>
2025-02-26 14:20:01 -0500

    dados

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,31 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner tecl = new Scanner(System.in);
+        Random ale = new Random();
 
-        int peso;
-        double estatura,total;
+        int a=0, dado1, dado2, total1=0, total2=0;
 
+        int lanzadas = ale.nextInt((100-2+1))+2;
+        System.out.println("Lanzadas: "+lanzadas+"\nDado 1 || Dado 2");
         do{
-            System.out.println("Ingrese su peso: ");
-            peso = tecl.nextInt();
-        }while(peso<=0);
-        do{
-            System.out.println("Ingrese su estatura: ");
-            estatura = tecl.nextDouble();
-        }while(estatura<=0);
-        total = peso/(estatura*estatura);
-        if(total<=18.49){
-            System.out.println("Su IMC es: "+total+"\nClasificación: Peso Bajo");
-        }else if(total<=24.99){
-            System.out.println("Su IMC es: "+total+"\nClasificación: Peso Normal");
-        }else if(total<=29.99){
-            System.out.println("Su IMC es: "+total+"\nClasificación: Sobrepeso");
-        }else if(total<=34.99){
-            System.out.println("Su IMC es: "+total+"\nClasificación: Obesidad leve");
-        }else if(total<=39.99){
-            System.out.println("Su IMC es: "+total+"\nClasificación: Obesidad mediana");
-        }else{
-            System.out.println("Su IMC es: "+total+"\nClasificación: Obesidad Mórbida");
+            dado1 = ale.nextInt((6-1+1))+1;
+            total1=dado1+total1;
+            dado2 = ale.nextInt((6-1+1))+1;
+            total2=dado2+total2;
+            System.out.println(" "+dado1+" || "+dado2);
+           a++;
+        }while(a<lanzadas);
+        System.out.println("TOTALES\nDado 1: "+total1+"\nDado 2: "+total2);
+        if(total1>total2){
+            System.out.println("Dado 1 es el GANADOR");
+        }else if(total2>total1){
+            System.out.println("Dado 2 es el GANADOR");
+        }else if(total1==total2){
+            System.out.println("EMPATE");
         }
+
     }
 }
