320e2eb9561832ef19fa5c52e1624edd5cb919ec
YulianDiaz <yulian.diaz-g@uniminuto.edu.co>
2025-03-12 13:45:06 -0500

    Cargar

--- src/Main.java
+++ src/Main.java
@@ -6,195 +6,112 @@ public class Main {
 
         Scanner dato = new Scanner(System.in);
         Random ale = new Random();
-
-        int opc=0,m=0;
-        double num, resultado;
+        int opc=0, tipo=0, l=0, d=0, c=0, totalLlamadas=0, m=0;
+        double local=50, distancia=350, celular=150, minutos=0, mLocal=0, mDistancia=0, mCelular=0, cLocal=0, cDistancia=0,cCelular=0, totalMinutos=0, totalCosto=0;
         do{
-        System.out.println(
-                "================\n      MENÚ      \n================"
-        +"\nCelsius a Fahrenheit->(1)\nCelsius a Kelvin->(2)\nCelsius a Rankine->(3)\nCelsius a Réaumur->(4)"
-        +"\nFahrenheit a Celsius->(5)\nFahrenheit a Kelvin->(6)\nFahrenheit a Rankine->(7)\nFahrenheit a Réaumur->(8)"
-        +"\nKelvin a Celsius->(9)\nKelvin a Fahrenheit->(10)\nKelvin a Rankine->(11)\nKelvin a Réaumur->(12)"
-        +"\nRankine a Celsius->(13)\nRankine a Fahrenheit->(14)\nRankine a Kelvin->(15)"
-        +"\nSalir->(16)");
-        opc = dato.nextInt();
+        System.out.println("==========\n   MENÚ   \n=========="
+        +"\nRegistrar Llamada ->(1)"
+        +"\nMostrar Información ->(2)"
+        +"\nReiniciar ->(3)"
+        +"\nSalir ->(4)");
+        opc= dato.nextInt();
         switch (opc){
-            case 1->{
-                do {
-                    System.out.println("Ingrese los Celcius: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
+            case 1 ->{
+                System.out.println(">>>REGISTRAR LLAMADA<<<"
+                        +"\nLocal ->(1)" +
+                        "\nLarga Diastancia ->(2)" +
+                        "\nCelular ->(3)");
+                tipo = dato.nextInt();
+                m=1;
+                switch (tipo){
+                    case 1 ->{
+                        do {
+                            System.out.println("Ingrese los minutos utlizados:");
+                            minutos = dato.nextDouble();
+                            if(minutos<0){
+                                System.out.println("Ingrese un valor valido...");
+                            }
+                        }while (minutos<0);
+                        mLocal=mLocal+minutos;
+                        cLocal=minutos*local;
+                        l++;
+                        System.out.println("Movimiento:"
+                        +"\nTotal minutos: "+mLocal
+                        +"\nCosto Llamada: "+cLocal);
                     }
-                }while(num<0);
-                    resultado = (num * (9 / 5)) + 32;
-                    System.out.println("El resultado es:\nFahrenheit (°F):" + resultado);
-            }
-            case 2->{
-                do {
-                System.out.println("Ingrese los Celcius: ");
-                num = dato.nextDouble();
-                if(num<0){
-                    System.out.println("Porfavor ingresa un número mayor o igual a cero...");
+                    case 2 ->{
+                        do {
+                            System.out.println("Ingrese los minutos utlizados:");
+                            minutos = dato.nextDouble();
+                            if(minutos<0){
+                                System.out.println("Ingrese un valor valido...");
+                            }
+                        }while (minutos<0);
+                        mDistancia=mDistancia+minutos;
+                        cDistancia=minutos*distancia;
+                        d++;
+                        System.out.println("Movimiento:"
+                                +"\nTotal minutos: "+mDistancia
+                                +"\nCosto Llamada: "+cDistancia);
+                    }
+                    case 3 ->{
+                        do {
+                            System.out.println("Ingrese los minutos utlizados:");
+                            minutos = dato.nextDouble();
+                            if(minutos<0){
+                                System.out.println("Ingrese un valor valido...");
+                            }
+                        }while (minutos<0);
+                        mCelular=mCelular+minutos;
+                        cCelular=minutos*celular;
+                        c++;
+                        System.out.println("Movimiento:"
+                                +"\nTotal minutos: "+mCelular
+                                +"\nCosto Llamada: "+cCelular);
+                    }
+                    default -> {
+                        System.out.println("Ingrece una opción valida....");
+                        m=1;
+                    }
                 }
-            }while(num<0);
-                resultado = num+273.15;
-                System.out.println("El resultado es:\nKelvin (K):"+resultado);
             }
-            case 3->{
-                do {
-                System.out.println("Ingrese los Celcius: ");
-                num = dato.nextDouble();
-                if(num<0){
-                    System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                }
-            }while(num<0);
-                resultado = (num+273.15)*(9/5);
-                System.out.println("El resultado es:\nRankine (°R):"+resultado);
-            }
-            case 4->{
-                do {
-                System.out.println("Ingrese los Celcius: ");
-                num = dato.nextDouble();
-        if(num<0){
-            System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-        }
-    }while(num<0);
-                resultado = num*(4/5);
-                System.out.println("El resultado es:\nRéaumur (°Re):"+resultado);
-            }
-            case 5->{
-                do {
-                    System.out.println("Ingrese los Fahrenheit: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = (num-32)*(5/9);
-                System.out.println("El resultado es:\nCelsius (°C):"+resultado);
-            }
-            case 6->{
-                do {
-                    System.out.println("Ingrese los Fahrenheit: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = ((num-32)*(5/9))+273.15;
-                System.out.println("El resultado es:\nKelvin (K):"+resultado);
-
-            }
-            case 7->{
-                do {
-                    System.out.println("Ingrese los Fahrenheit: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = num+459.67;
-                System.out.println("El resultado es:\nRankine (°R):"+resultado);
-
-            }
-            case 8->{
-                do {
-                    System.out.println("Ingrese los Fahrenheit: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = (num-32)*(4/9);
-                System.out.println("El resultado es:\nRéaumur (°Re):"+resultado);
-
-            }
-            case 9->{
-                do {
-                    System.out.println("Ingrese los Kelvin: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = num-273.15;
-                System.out.println("El resultado es:\nCelcius (°C):"+resultado);
-            }
-            case 10->{
-                do {
-                    System.out.println("Ingrese los Kelvin: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = ((num-273.15)*(9/5))+32;
-                System.out.println("El resultado es:\nFahrenheit (°F):"+resultado);
-            }
-            case 11->{
-                do {
-                    System.out.println("Ingrese los Kelvin: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = num*(9/5);
-                System.out.println("El resultado es:\nRankine (°R):"+resultado);
-            }
-            case 12->{
-                do {
-                    System.out.println("Ingrese los Kelvin: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = (num-273.15)*(4/5);
-                System.out.println("El resultado es:\nRéaumur (°Re):"+resultado);
-            }
-            case 13->{
-                do {
-                    System.out.println("Ingrese los Rankine: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = (num-491.67)*(5/9);
-                System.out.println("El resultado es:\nCelcius (°C):"+resultado);
-            }
-            case 14->{
-                do {
-                    System.out.println("Ingrese los Rankine: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = num-459.67;
-                System.out.println("El resultado es:\nFahrenheit (°F):"+resultado);
-            }
-            case 15->{
-                do {
-                    System.out.println("Ingrese los Rankine: ");
-                    num = dato.nextDouble();
-                    if(num<0){
-                        System.out.println("Porfavor ingresa un número mayor o igual a cero...");
-                    }
-                }while(num<0);
-                resultado = num*(5/9);
-                System.out.println("El resultado es:\nKelvin (K):"+resultado);
-            }
-            case 16->{
-                System.out.println("Gracias por usar nuestra calculadora....");
+            case 2 ->{
+                totalMinutos=mLocal+mDistancia+mCelular;
+                totalLlamadas=l+d+c;
+                totalCosto=cLocal+cDistancia+cCelular;
+                System.out.println(
+                        "\nLocal:" +
+                        "\nMinutos: "+mLocal+
+                        "\nCosto: "+cLocal+
+                        "\nLlamadas: "+l+
+                        "\nLarga Distancia:" +
+                        "\nMinutos: "+mDistancia+
+                        "\nCosto: "+cDistancia+
+                        "\nLlamadas: "+d+
+                        "\nCelular:" +
+                        "\nMinutos: "+mCelular+
+                        "\nCosto: "+cCelular+
+                        "\nLlamadas: "+c+
+                        "\nTotal:" +
+                        "\nMinutos totales: "+totalMinutos+
+                        "\nCosto totales: "+totalCosto+
+                        "\nLlamadas totales: "+totalLlamadas);
                 m=1;
             }
-            default -> {
-                System.out.println("Ingrese una opción valida...");
+            case 3 ->{
+                l=0; d=0; c=0; totalLlamadas=0; m=0;
+                minutos=0; mLocal=0; mDistancia=0; mCelular=0; cLocal=0; cDistancia=0;cCelular=0; totalMinutos=0; totalCosto=0;
+                System.out.println("Se reinicio con exito...");
+                m=1;
             }
+            case 4 ->{
+                m=0;
+            }
+            default -> {
+                System.out.println("Ingrece una opción valida....");
+                m=1;
+            }
+        }}while(m==1);
 
-        }}while(m==0);
     }
 }
